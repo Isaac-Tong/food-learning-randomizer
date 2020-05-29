172ef11519ad2ac4d715a8d21e68f0021b852979
Isaac Tong <isaact943@outlook.com>
2020-05-29 11:07:23 +0800

    randomize page structure completed

---
+++ learning_food_randomizer/lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+const kLightPink = Color(0xFFe9aacb);
+const kPurple = Color(0xFFc295d8);
--- learning_food_randomizer/lib/screens/randomize.dart
+++ learning_food_randomizer/lib/screens/randomize.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learningfoodrandomizer/constants.dart';
 
 class Randomize extends StatefulWidget {
   @override
@@ -8,8 +10,74 @@ class Randomize extends StatefulWidget {
 class _RandomizeState extends State<Randomize> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 6,
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: (){
+                            print('asd');
+                          },
+                          child: Image(
+                            width: 130,
+                            image: AssetImage('assets/cookbook_image.png'),
+                          ),
+                        ),
+                        Text(
+                          'click the cookbook to begin!',
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: kPurple,
+                            fontFamily: 'Lato',
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.all(40),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [kPurple, kLightPink],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  color: kLightPink,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(25),
+                      topRight: Radius.circular(25)),
+                ),
+                child: Text(
+                  'Randomize my food',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                    fontFamily: 'Lato',
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
