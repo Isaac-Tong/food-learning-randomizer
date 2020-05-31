2db6efbae527b7c7dbb654074d2a523fa6c05c1e
Isaac Tong <isaact943@outlook.com>
2020-05-31 15:08:41 +0800

    Moved bing image authentication to .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'screens/randomize.dart';
 import 'screens/show_food.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await DotEnv().load('.env');
   runApp(MyApp());
 }
 
--- lib/screens/show_food.dart
+++ lib/screens/show_food.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ShowFood extends StatefulWidget {
   @override
@@ -16,6 +17,15 @@ class _ShowFoodState extends State<ShowFood> {
     return decodedData;
   }
 
+  Future getImage() async{
+    http.Response imageResponse = await http.get('https://eastasia.api.cognitive.microsoft.com/bing/v7.0/images/search?q="Burger"&count=1', headers: {'Ocp-Apim-Subscription-Key': DotEnv().env['BING_IMAGES_AUTH']});
+    Map decodedResponse = json.decode(imageResponse.body);
+    //Convert map to a list
+
+
+    print(decodedResponse['value'][0]['contentUrl']);
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -24,11 +34,6 @@ class _ShowFoodState extends State<ShowFood> {
   }
 
   List<Widget> items = [
-    Container(child: Text('Hi'),),
-    Container(child: Text('Hi'),),
-    Container(child: Text('Hi'),),
-    Container(child: Text('Hi'),),
-    Container(child: Text('Hi'),),
   ];
 
   @override
@@ -40,6 +45,7 @@ class _ShowFoodState extends State<ShowFood> {
             MaterialButton(
               color: Colors.green,
                 onPressed: () async{
+                getImage();
                 var data = await getData();
                 var food = data['food_name'];
               setState(() {
