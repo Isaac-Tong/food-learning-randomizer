6c683ce5b5a59ab73a3378d77b0aeb5d59d2bfaf
Isaac Tong <isaact943@outlook.com>
2020-06-02 13:14:07 +0800

    PageView successfully implemented

--- lib/screens/random_food_screen/test_pageview.dart
+++ lib/screens/random_food_screen/test_pageview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learningfoodrandomizer/constants.dart';
+import 'package:learningfoodrandomizer/functions/http_functions.dart';
 
 class DisplayPage extends StatefulWidget {
   @override
@@ -375,12 +376,16 @@ class _DisplayPageState extends State<DisplayPage> {
             flex: 5,
             child: Padding(
               padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-              child: PageView(
+              child: PageView.builder(
                 onPageChanged: (idx){
                   index = idx;
                 },
+                pageSnapping: true,
+                itemCount: 2,
                 controller: pageController,
-                children: newPage,
+                itemBuilder: (context, position){
+                  return SinglePageWidget();
+                },
               ),
             ),
           ),
@@ -461,7 +466,7 @@ class _DisplayPageState extends State<DisplayPage> {
                             ),
                           ],
                         ),);
-                        pageController.animateToPage(index++, duration: Duration(milliseconds: 300), curve: Curves.easeInCubic);
+                        pageController.animateToPage(2, duration: Duration(milliseconds: 300), curve: Curves.easeInCubic);
                       });
                     },
                     child: Container(
@@ -500,6 +505,120 @@ class _DisplayPageState extends State<DisplayPage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class SinglePageWidget extends StatefulWidget {
+  const SinglePageWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _SinglePageWidgetState createState() => _SinglePageWidgetState();
+}
+
+class _SinglePageWidgetState extends State<SinglePageWidget> {
+
+  //Text And Image Variables
+  String mainText = 'Loading';
+  String cuisineText = 'Loading';
+  String foodURL = 'https://www.heart.org/-/media/images/health-topics/congenital-heart-defects/50_1683_44a_asd.jpg?h=551&w=572&la=en&hash=60A4E57B316F13921A743143171BD2EFC7912F93';
+
+  void setData() async {
+    Map apiData = await getData();
+    //Decode the food_name from the map
+
+    setState((){
+      mainText = apiData['food_name'];
+      cuisineText = apiData['type'];
+
+    });
+
+    String returnedURL = await getImage(mainText);
+
+    setState(() {
+      foodURL = returnedURL;
+    });
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setData();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.all(25),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(7, 7),
+                      blurRadius: 30),
+                ]),
+            width: double.infinity,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FittedBox(
+                fit: BoxFit.fitHeight,
+                child: Image(
+                  image: NetworkImage(foodURL),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(25, 0, 25, 25),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    FittedBox(
+                      fit: BoxFit.contain,
+                      child: Text(
+                        mainText,
+                        style: TextStyle(
+                            color: kPurple,
+                            fontWeight: FontWeight.w900,
+                            fontSize: 42),
+                      ),
+                    ),
+                    Text(
+                      cuisineText,
+                      style: TextStyle(
+                        color: kLightPink,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Image(
+                    image: AssetImage('assets/cookbook_image.png'),
+                    width: 20),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
